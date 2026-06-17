b16b43145ed6b59aed55010fbce7ea81c32319f7
musananjireddy <musananjireddy2006@gmail.com>
2026-06-17 09:47:12 +0530

    Time: 3 ms (75.11%) | Memory: 16.2 MB (89.23%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -3,13 +3,13 @@ public:
     string largestNumber(vector<int>& nums) {
         string res="";
         sort(nums.begin(),nums.end(),[](int a,int b){
-            return (to_string(a)+to_string(b))>(to_string(b)+to_string(a));
+            return to_string(a)+to_string(b)>to_string(b)+to_string(a);
         });
         for(int x:nums){
-            string s = to_string(x);
-            res+=s;
+            res+= to_string(x);
+            
         }
-        if(res[0]=='0'){
+        if(res[0]=='0' ){
             return "0";
         }
         return res;
