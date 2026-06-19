f29747a8d1fc6d919edc72e1eb19281fa853c7da
musananjireddy <musananjireddy2006@gmail.com>
2026-06-19 21:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,36 +4,29 @@ public:
         int  n = nums.size();
         int i=0;
         int j=n-1;
-        while(i<j){
+        while(i<=j){
             int  mid= (i+j)/2;
-            if(nums[mid]>nums[j]){
-                i = mid+1;
+            if(nums[mid]==target){
+                return mid;
             }
-            else{
-                j = mid;
-            }
-        }
-            int p=i;
-            if(nums[p]<=target && nums[n-1]>=target){
-                i=p;
-                j=n-1;
-            }
-            else{
-                i=0;
-                j=p-1;
-            }
-            while(i<=j){
-                int mid = (i+j)/2;
-                if(nums[mid]==target){
-                    return mid;
+            else if(nums[i]<=nums[mid]){
+                if(nums[i]<=target && target<nums[mid]){
+                    j =mid-1;
                 }
-                else if(nums[mid]<target){
+                else{
+                    i = mid+1;
+                }
+            }
+            else{
+                if(target<=nums[j] && target>nums[mid]){
                     i=mid+1;
                 }
                 else{
                     j=mid-1;
                 }
+
             }
+        }
             return -1;
     }
 };
