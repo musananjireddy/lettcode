70e3660a4fec3da7b3f01129b2b4dfd94051aa85
musananjireddy <musananjireddy2006@gmail.com>
2026-06-21 17:48:12 +0530

    Time: 42 ms (19.75%) | Memory: 86.8 MB (9.95%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -4,13 +4,15 @@ public:
         int n = costs.size();
         sort(costs.begin(),costs.end());
         int count=0;
-        long long sum=0;
+        vector<long long>prefix(n,0);
+        prefix[0]=costs[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+costs[i];
+        }
         for(int i=0;i<n;i++){
-            sum+=costs[i];
-            if(sum<=coins){
+            if(prefix[i]<=coins){
                 count++;
             }
-
         }
         return count;
     }
