283d768ff9f4ab3f473bd010aeda0bc37f1e0201
musananjireddy <musananjireddy2006@gmail.com>
2026-08-06 19:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (46.68%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,49 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        string res1="";
-        while(l1!=nullptr){
-            res1+=char(l1->val+'0');
-            l1=l1->next;
-        }
-        string  res2 = "";
-        while(l2!=nullptr){
-            res2+=char(l2->val+'0');
-            l2=l2->next;
+        ListNode*dummy = new ListNode(0);
+        ListNode*curr = dummy;
+        int carry=0;
+        while(l1!=nullptr || l2!=nullptr || carry!=0){
+            int sum=0;
+            if(l1!=nullptr){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                sum+=l2->val;
+                l2 = l2->next;
+            }
+            sum+=carry;
+            curr->next = new ListNode((sum%10));
+            curr=curr->next;
+            carry = sum/10;
             
         }
-       string str ="";
-       int i=0;
-       int j = 0;
-       int carry=0;
-       while(i<res1.size() || j<res2.size() || carry){
-        int x=0;
-        int y=0;
-        if(i<res1.size()){
-            x =res1[i]-'0';
-        }
-        if(j<res2.size()){
-            y = res2[j]-'0';
-        }
-        int sum  =x+y+carry;
-        str+=char((sum%10)+'0');
-        carry = sum/10;
-        i++;
-        j++;
-       }
-
-        ListNode* newval = new ListNode(str[0] - '0');
-        ListNode* temp = newval;
-
-        for (int k = 1; k<str.size(); k++) {
-            ListNode* newnode = new ListNode(str[k] - '0');
-            temp->next = newnode;
-            temp = temp->next;
-        }
-
-        return newval;
-
-        
+        return dummy->next;
         
     }
 };
