9a57cee98714b075a975da34819ac9d279d91c98
musananjireddy <musananjireddy2006@gmail.com>
2026-08-06 19:03:09 +0530

    Time: 3 ms (34.17%) | Memory: 77.2 MB (13.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,61 +11,41 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode*prev= nullptr;
-        ListNode*curr = l1;
-        ListNode*next = l1;
-        while(curr!=nullptr){
-            next=next->next;
-            curr->next = prev;
-            prev=curr;
-            curr = next;
-        }
-        ListNode*pre= nullptr;
-        ListNode*cur = l2;
-        ListNode*nex = l2;
-        while(cur!=nullptr){
-            nex=nex->next;
-            cur->next = pre;
-            pre=cur;
-            cur = nex;
-        }
         string res1="";
-        while(prev!=nullptr){
-            res1+=char(prev->val+'0');
-            prev=prev->next;
+        while(l1!=nullptr){
+            res1+=char(l1->val+'0');
+            l1=l1->next;
         }
         string  res2 = "";
-        while(pre!=nullptr){
-            res2+=char(pre->val+'0');
-            pre=pre->next;
+        while(l2!=nullptr){
+            res2+=char(l2->val+'0');
+            l2=l2->next;
             
         }
        string str ="";
-       int i=res1.size()-1;
-       int j = res2.size()-1;
+       int i=0;
+       int j = 0;
        int carry=0;
-       while(i>=0 || j>=0 || carry){
+       while(i<res1.size() || j<res2.size() || carry){
         int x=0;
         int y=0;
-        if(i>=0){
+        if(i<res1.size()){
             x =res1[i]-'0';
         }
-        if(j>=0){
+        if(j<res2.size()){
             y = res2[j]-'0';
         }
         int sum  =x+y+carry;
         str+=char((sum%10)+'0');
         carry = sum/10;
-        i--;
-        j--;
+        i++;
+        j++;
        }
-       reverse(str.begin(),str.end());
-               int n = str.size();
 
-        ListNode* newval = new ListNode(str[n - 1] - '0');
+        ListNode* newval = new ListNode(str[0] - '0');
         ListNode* temp = newval;
 
-        for (int k = n - 2; k >= 0; k--) {
+        for (int k = 1; k<str.size(); k++) {
             ListNode* newnode = new ListNode(str[k] - '0');
             temp->next = newnode;
             temp = temp->next;
