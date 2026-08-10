bc5d9da2d9fc3d402784d245da5c33a3b6f8929f
musananjireddy <musananjireddy2006@gmail.com>
2026-08-10 07:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (93.53%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     string decodeString(string s) {
-      stack<int>num;
-      stack<string>str;
-      int len=0;
-      string res="";
-      for(int i=0;i<s.size();i++){
-        if(isdigit(s[i])){
-            len=0;
-            while(isdigit(s[i])){
-                len= len*10+(s[i]-'0');
-                i++;
-            }
-            i--;
-        }
-        else if(s[i]=='['){
-            num.push(len);
-            str.push(res);
-            res="";
-            len=0;
-        }
-        else if(s[i]==']'){
-            string temp=res;
-            res=str.top();
-            str.pop();
-            int k=num.top();
-            num.pop();
-            while(k--){
-                res+=temp;
-            }
+        string res="";
+        stack<int>num;
+        stack<string>str;
+        string temp="";
+        int len;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                len = 0;
+                while(isdigit(s[i])){
+                    len = len*10+s[i]-'0';
+                    i++;
+                }
+                i--;
 
+            }
+           
+            else if(s[i]=='['){
+                num.push(len);
+                str.push(res);
+                len=0;
+                res="";
+            }
+            else if(s[i]==']'){
+                temp=res;
+                res = str.top();
+                str.pop();
+                int k = num.top();
+                num.pop();
+                while(k>0){
+                    res+=temp;
+                    k--;
+                }
+            }
+            else{
+                res+=s[i];
+            }
         }
-        else{
-            res+=s[i];
-        }
-      }
-      return res;
+        return res;
+        
     }
 };
